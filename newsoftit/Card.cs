0b9d247087bf745eb878e6cd69789bd2f1ee77de
kk <nck1987@gmail.com>
2022-03-28 13:44:21 +0800

    Add project files.

---
+++ newsoftit/Card.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace newsoftit
+{
+    public enum Suit
+    {
+        Clubs,
+        Hearts,
+        Spades,
+        Diamonds
+    }
+
+    public enum Rank
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public Rank Rank { get; set; }
+    }
+}
---
+++ newsoftit/Deck.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace newsoftit
+{
+    public class Deck
+    {
+        public Deck()
+        {
+        }
+
+        public Suit getSuit(string suit)
+        {
+            switch (suit)
+            {
+                case "h":
+                    return Suit.Hearts;
+                case "s":
+                    return Suit.Spades;
+                case "d":
+                    return Suit.Diamonds;
+                case "c":
+                    return Suit.Clubs;
+                default:
+                    throw new Exception($"Suit {suit} not found.");
+            }
+        }
+        public Rank getRank(string rank)
+        {
+            switch (rank)
+            {
+                case "2":
+                    return Rank.Two;
+                case "3":
+                    return Rank.Three;
+                case "4":
+                    return Rank.Four;
+                case "5":
+                    return Rank.Five;
+                case "6":
+                    return Rank.Six;
+                case "7":
+                    return Rank.Seven;
+                case "8":
+                    return Rank.Eight;
+                case "9":
+                    return Rank.Nine;
+                case "10":
+                    return Rank.Ten;
+                case "J":
+                    return Rank.Jack;
+                case "Q":
+                    return Rank.Queen;
+                case "K":
+                    return Rank.King;
+                case "A":
+                    return Rank.Ace;
+                default:
+                    throw new Exception($"card Rank {rank} not found.");
+            }
+        }
+    }
+}
